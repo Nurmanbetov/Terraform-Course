7d2914fa0bbebc1482e7be28b9bc7d955fa99327
Nurmanbetov <nurmanbetov.594@gmail.com>
2023-07-05 17:52:52 +0200

    section 5 with .tf loop output

---
+++ section5/c1-versions.tf
@@ -0,0 +1,17 @@
+#Terrform Block
+
+terraform {
+  required_version = "~> 1.5.0"
+  required_providers {
+    aws = {
+        source = "hashicorp/aws"
+        version = "~> 3.0"
+    }
+  }
+}
+
+#Provider Block
+provider "aws" {
+    region = ""
+#   region = var.aws_region
+}
